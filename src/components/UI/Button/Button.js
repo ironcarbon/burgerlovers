58751e970d5dfb43196451a9c0fcd8c6521699df
ipek <ipekcivicakan@gmail.com>
2018-11-02 17:03:45 -0700

    Order Summary, Order Button added

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Button.css';
+
+const button = (props) => (
+    <button 
+        className={[classes.Button, classes[props.btnType]].join(' ')}
+        //className={`${classes.Button}, ${classes[props.btnType]}`}
+        onClick={props.clicked}>{props.children}</button>
+);
+
+export default button;
